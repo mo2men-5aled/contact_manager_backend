47f4aebe20f17890e25ece983d79e30e8219edf9
Momen Khaled <momenk208@gmail.com>
2025-06-05 05:12:08 +0300

    feat: add Swagger documentation for API endpoints and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,23 @@ import contactRoutes from './routes/contact.route';
 import { verifyToken } from './middlewares/auth.middleware';
 import { errorHandler } from './middlewares/errorHandler.middleware';
 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
+const swaggerDocument = YAML.load(path.join(__dirname, 'docs/swagger.yaml'));
+
+console.log('Swagger Document Loaded:', swaggerDocument);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/contacts', verifyToken, contactRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
