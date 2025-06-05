90a4219fdf4d566b1c15ada5eaa520d9754ec4b7
Momen Khaled <momenk208@gmail.com>
2025-06-05 04:18:28 +0300

    feat: add error handling middleware to manage application errors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.route';
 import contactRoutes from './routes/contact.route';
+
 import { verifyToken } from './middlewares/auth.middleware';
+import { errorHandler } from './middlewares/errorHandler.middleware';
 
 const app = express();
 
@@ -12,5 +14,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/contacts', verifyToken, contactRoutes);
+
+app.use(errorHandler);
 
 export default app;
