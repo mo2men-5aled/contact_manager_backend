6b726f51bf4bd39deca4d5b71e7f8b47c3e78270
Momen Khaled <momenk208@gmail.com>
2025-06-11 17:13:46 +0300

    updated user roles

--- src/middlewares/role.middleware.ts
+++ src/middlewares/role.middleware.ts
@@ -4,6 +4,7 @@ export const authorize =
   (allowedRoles: string[]) =>
   (req: Request, res: Response, next: NextFunction) => {
     const user = req.user;
+    console.log(user);
 
     if (!user || !allowedRoles.includes(user.role)) {
       res.status(403).json({ message: 'Access denied: insufficient rights' });
